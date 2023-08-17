9e6a38517f95894d159d04193313dbc7098dae0f
Rose Lalk <rose.lalk@kvk.nl>
2023-08-17 13:28:36 +0200

    Removed some useless comments from Nav.jsx. This is a test branch to test out the Pull Request functionality on GitHub.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,6 @@ function Nav() {
     return (
         <nav className="nav-container">
             <h2 className="active">ALL TASKS</h2>
-            {/* Het idee is om hier een shortcut te hebben met open/doing/done stories, en dan die pagina's
-            ook hun eigen uiterlijk/UX te geven. */}
-            {/* <h2>OPEN</h2>
-            <h2>DOING</h2>
-            <h2>DONE</h2> */}
         </nav>
   );
 }
