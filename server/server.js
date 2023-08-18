fa07da849a07f00beb8b1b28bfd9918d8aef62e0
Rose Lalk <rose.lalk@kvk.nl>
2023-08-18 15:33:39 +0200

    Implemented functionality so files can be read from the XMLfiles directory, are then converted to JSON using xml2js, and then saved as a JSON object in a JSON file inside of the JSONfiles directory. The XML file is then moved to the Archive folder.

--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
 const fs = require("fs");
+const path = require("path");
 
 const xml2js = require("xml2js");
 const parser = new xml2js.Parser();
 
-const timerId = setInterval(() => {
+const moveFrom = "./XMLfiles"
+const moveTo = "./Archive"
+
+const XMLtoJSONconverter = async () => {
   try {
-    getXML();
-  } catch (error) {
-    console.log(error);
+
+    const files = await fs.promises.readdir("./XMLfiles");
+
+    for (const file of files) {
+
+      const fromPath = path.join(moveFrom, file);
+      const toPath = path.join(moveTo, file);
+
+      fs.readFile(file, function (err, data) {
+
+        const trimmedFileName = file.replace(/\.[^/.]+$/, "");
+        
+        parser.parseString(data, (result) => {
+          fs.writeFile(`./JSONfiles/${trimmedFileName}.json`, JSON.stringify(result, null, 2), 'utf-8', (err, result) => {})
+        });
+      });
+
+      await fs.promises.rename(fromPath, toPath);
+    }
+  } catch (e) {
+    console.error("Oops, something went wrong!");
   }
-}, 500);
-
-// setTimeout(() => {
-//   clearInterval(timerId);
-
-//   console.log("stop");
-// }, 250);
-
-const getXML = async () => {
-  fs.readFile(__dirname + "/test.xml", function (err, data) {
-    parser.parseString(data, function (err, result) {
-      console.dir(result.rss.channel);
-      console.dir(result.rss.channel[0].item);
-      console.log("Done");
-    });
-  });
 };
 
-// iedere 500ms checken of map /server/xml-files/ nieuw bestand(en)
-// indien nieuw bestand: uitlezen met xml2js
-// op basis van jira id kijken of deze al bestaat in /server/json-files/
-// indien wel bestaand: oude deleten en nieuw JSON object maken en daar alleen relevante informatie in opslaan
-// indien niet bestaand: nieuw JSON object maken en daar alleen relevante informatie in opslaan
-// xml bestand verwijderen
+XMLtoJSONconverter();
 
-// los een API service maken die aangeroepen kan worden en alle JSON files exposed
